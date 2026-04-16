af455efce106815c75097abc28e1803ae7960caa
Simon9050 <nos3@nos3>
2026-04-16 17:29:20 +0200

    clean french comments in estrattore

--- Desktop/github-nos3/estrattore.sh
+++ Desktop/github-nos3/estrattore.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-
- 
-
-# Fonction pour extraire le nom de la fonction englobante
+# look for IRT CSS pattern
 
 extract_function_name() {
     local file="$1"
     local line_number="$2"
 
-    # Génère les tags dans un format lisible
     ctags -x "$file" 2>/dev/null | sort -k2n | tac  |\
     awk -v line="$line_number" '$2 <= line { print $1 }' | \
     head -n 1
 }
 
- 
 
-# Parcours des fichiers .c et .h
 
 rm /tmp/_listModif
 
 find . -type f \( -name "*.c" -o -name "*.h" \) | while read -r file; do
 
-    # Recherche du motif IRT CSS
+    # look for IRT CSS pattern
 
     grep -n -E 'IRT.*CSS' "$file" | while IFS=: read -r line_number line_content; do
     #grep -n "IRT CSS" "$file" | while IFS=: read -r line_number line_content; do
