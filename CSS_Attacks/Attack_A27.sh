b447f57c75998f5b85936fa03003a2b7b7329b4b
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2026-04-17 11:20:38 +0200

    fix: remove unused code

--- CSS_Attacks/Attack_A27.sh
+++ CSS_Attacks/Attack_A27.sh
@@ -6,4 +6,3 @@ else
 	#no arg provided or bad syntax, default behavior
 	python3 ../Input_Generator/Main.py send ../Input_Generator/scenarios/SW_1.json
 fi
-#python3 ../Input_Generator/Main.py send ../Input_Generator/scenarios/SW_1.json
